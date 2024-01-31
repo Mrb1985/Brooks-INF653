7e8e0edcbab8590f7defcfb003d926346c8b7fb8
Mrb1985 <122954943+Mrb1985@users.noreply.github.com>
2024-01-30 18:06:35 -0700

    Update Matthew Brooks Project 1.php

--- Matthew Brooks Project 1.php
+++ Matthew Brooks Project 1.php
@@ -1,44 +1,44 @@
 <?php
 
-// Clean the input parameters to prevent XSS attacks
+// filter parameters for XSS attacks
 $firstname = filter_input(INPUT_GET, 'firstname', FILTER_SANITIZE_STRING);
 $lastname = filter_input(INPUT_GET, 'lastname', FILTER_SANITIZE_STRING);
 $age = filter_input(INPUT_GET, 'age', FILTER_SANITIZE_NUMBER_INT);
 
-// Check if all required parameters are present
+// Check if parameters exist
 if (empty($firstname) || empty($lastname) || empty($age)) {
-  // Display an error message if any of the required parameters are missing
-  echo '<h1>Missing required parameters</h1>';
+  echo '<h1>Missing parameters</h1>';
   exit;
 }
 
-// Calculate the days based on the number given for age
+// Calculate days
 $days = $age * 365;
 
-// Add the current date to the page
+// Current date
 $date = date('Y-m-d');
 
-// Output the two sentences
 
+// Output name
 echo 'Hello, my name is ' . $firstname . ' ' . $lastname . '.';
 
-// Output the conditional statement based on the age parameter
+// Output age along with string
 if ($age >= 18) {
   echo ' I am ' . $age . ' years old and I am old enough to vote in the United States.';
 } else {
   echo ' I am ' . $age . ' years old and I am not old enough to vote in the United States.';
 }
 
-echo '</p>';
 
-// Output the third sentence
-echo '<p>';
+
+// Output number of days
+
 echo   $days . ' days.';
-echo '</p>';
 
-// Output the current date
-echo '<p>';
-echo 'The current date is ' . $date . '.';
-echo '</p>';
+
+// Output date
+
+echo 'The date is ' . $date . '.';
+
 
 ?>
+
